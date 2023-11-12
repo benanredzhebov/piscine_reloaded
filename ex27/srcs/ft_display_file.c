e02aa6a45a4103bd55a6634dd7d50dc4757799d9
beredzhe <beredzhe@student.42wolfsburg.de>
2023-11-12 23:19:23 +0100

    La

--- ex27/srcs/ft_display_file.c
+++ ex27/srcs/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: beredzhe <beredzhe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 21:30:26 by beredzhe          #+#    #+#             */
-/*   Updated: 2023/11/12 22:25:43 by beredzhe         ###   ########.fr       */
+/*   Updated: 2023/11/12 23:17:28 by beredzhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ void	display(char *filename)
 
 	file = open(filename, O_RDONLY);
 	if (file < 0)
+	{
+		write(1, "Cannot read file.\n", 19);
 		return ;
+	}
 	while (read(file, &character, 1))
 		write(1, &character, 1);
 	close(file);
